f41c0703227121acea6c7460e031fb9d591b7935
Christina Whethers <flandresamarawr@gmail.com>
2022-02-12 08:21:05 -0500

    Setup: Add initial Spring setup

---
+++ src/main/java/com/example/sxmpchowseeker/SxmpChowseekerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sxmpchowseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxmpChowseekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SxmpChowseekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sxmpchowseeker/SxmpChowseekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sxmpchowseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SxmpChowseekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
