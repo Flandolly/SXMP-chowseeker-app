5ac53a387adad3ca8b23908e47ca63544637408f
Christina Whethers <flandresamarawr@gmail.com>
2022-02-13 16:48:01 -0500

    Update Restaurant model to match seed data format

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -4,23 +4,33 @@ CREATE TABLE restaurants
 (
     id UUID,
     name VARCHAR(3000),
+    location_description VARCHAR(3000),
     address VARCHAR(3000),
+    photo VARCHAR(3000),
     food_types VARCHAR(3000),
-    locationx FLOAT,
-    locationy FLOAT,
+    latitude FLOAT,
+    longitude FLOAT,
+    schedule VARCHAR(3000),
     location_exact VARCHAR(3000),
+    likes INTEGER,
+    dislikes INTEGER,
     PRIMARY KEY (id)
 );
 
 INSERT INTO restaurants
-    (id, name, address, food_types, locationx, locationy, location_exact)
+    (id, name, location_description, address, photo, food_types, latitude, longitude, schedule, location_exact, likes, dislikes)
     VALUES
     (
         uuid_generate_v1(),
         'Test',
+        'Somewhere',
         '123 Test Street',
+        'google.com',
         'Food:Food2:Food3',
         69.69,
         420.69,
-        '(69.6969696969, 420.6969696969)'
+        'pdf link',
+        '(69.6969696969, 420.6969696969)',
+        69,
+        420
     );
