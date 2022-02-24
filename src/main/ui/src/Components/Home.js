dbaa09c67725b9319ff88b7f73c3744bc6f6971a
Christina Whethers <flandresamarawr@gmail.com>
2022-02-23 21:28:33 -0500

    Display a skeleton home screen with Home component

--- src/main/ui/src/Components/Home.js
+++ src/main/ui/src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import {APIURL} from "../config/config";
+import {Button, FormGroup, Input} from "reactstrap";
 
 function Home() {
 
@@ -16,8 +17,15 @@ function Home() {
     });
 
     return (
-        <div>
-            <h1>Restaurants</h1>
+        <div className={"container-fluid d-flex"}>
+            <div className={"d-flex flex-column vh-100 w-100 justify-content-center"}>
+                <h1 className={"text-center d-inline"}>Chowseeker</h1>
+                <h5 className={"text-center"}>Find Your Favorite SF Food Trucks!</h5>
+                <FormGroup>
+                    <Input className={"w-75 mx-auto"} type={"search"} name={"search"} placeholder={"Search..."}/>
+                </FormGroup>
+                <Button className={"w-25 mx-auto my-2"} type={"submit"} name={"submitSearch"} color={"primary"}>Submit</Button>
+            </div>
         </div>
     );
 }
