9beedaedf08c6524eabac4d2f976cf9e3d11ccb7
Christina Whethers <flandresamarawr@gmail.com>
2022-03-02 18:41:11 -0500

    Refactor header into own component, use loading bar for results

--- src/main/ui/src/Components/ResultList.js
+++ src/main/ui/src/Components/ResultList.js
@@ -48,14 +48,39 @@ function ResultList(props) {
             });
     }, [props.history.location.search])
 
+    useEffect(() => {
+        console.log(sort);
+        const sortResults = [...resultList].sort((a, b) => {
+            if (sort.sortDirection === "ascending") {
+                if (sort.sortParam === "name") {
+                    return a.name > b.name ? 1 : -1;
+                } else {
+                    return a.address > b.address ? 1 : -1;
+                }
+            }
+
+            if (sort.sortDirection === "descending") {
+                if (sort.sortParam === "name") {
+                    return a.name < b.name ? 1 : -1;
+                } else {
+                    return a.address < b.address ? 1 : -1;
+                }
+            }
+            return [];
+        })
+        setResultList(sortResults);
+    }, [sort])
+
+    useEffect(() => {
+    }, [resultList])
+
     function sortResults(event) {
         if (!sort.sortDirection || !sort.sortParam || sort.sortParam !== event.childNodes[0].nodeValue.toLowerCase()) {
             if (event.innerText.toLowerCase().includes("address")) {
                 setShowAddAsc(true);
                 setShowNameAsc(false);
                 setShowNameDesc(false);
-            }
-            else {
+            } else {
                 setShowNameAsc(true);
                 setShowAddAsc(false);
                 setShowAddDesc(false);
@@ -67,8 +92,7 @@ function ResultList(props) {
             if (event.innerText.toLowerCase().includes("address")) {
                 setShowAddAsc(false);
                 setShowAddDesc(true);
-            }
-            else {
+            } else {
                 setShowNameAsc(false);
                 setShowNameDesc(true);
             }
@@ -79,8 +103,7 @@ function ResultList(props) {
             if (event.innerText.toLowerCase().includes("address")) {
                 setShowAddAsc(true);
                 setShowAddDesc(false);
-            }
-            else {
+            } else {
                 setShowNameAsc(true);
                 setShowNameDesc(false);
             }
@@ -88,63 +111,54 @@ function ResultList(props) {
         }
     }
 
-    useEffect(() => {
-        console.log(sort);
-        const sortResults = [...resultList].sort((a, b) => {
-            if (sort.sortDirection === "ascending") {
-                if (sort.sortParam === "name") {
-                    return a.name > b.name ? 1 : -1;
-                }
-                else {
-                    return a.address > b.address ? 1 : -1;
-                }
-            }
-
-            if (sort.sortDirection === "descending") {
-                if (sort.sortParam === "name") {
-                    return a.name < b.name ? 1 : -1;
-                }
-                else {
-                    return a.address < b.address ? 1 : -1;
-                }
-            }
-            return [];
-        })
-        setResultList(sortResults);
-    }, [sort])
-
-    useEffect(() => {
-
-    }, [resultList])
-
-
-
-    return (
-        <div>
-            <h1 className={"display-6 mx-3"}>Showing search results for
-                "{props.history.location.search.substring(props.history.location.search.indexOf('=') + 1)}"</h1>
-            <p className={"lead mx-3"}>{resultList.length} results found.</p>
-            <div className={"lead text-end"}>
-                Sort By: <ButtonGroup>
-                <Button onClick={(event => sortResults(event.target))}>Address
-                    <span style={{display: showAddAsc ? "inline" : "none"}} id={"sort-asc-address"}> &#8593;</span>
-                    <span style={{display: showAddDesc ? "inline" : "none"}} id={"sort-desc-address"}> &#8595;</span>
-                </Button>
-                <Button onClick={(event => sortResults(event.target))}>Name
-                    <span style={{display: showNameAsc ? "inline" : "none"}} id={"sort-asc-name"}> &#8593;</span>
-                    <span style={{display: showNameDesc ? "inline" : "none"}} id={"sort-desc-name"}> &#8595;</span>
-                </Button>
-            </ButtonGroup>
+    function ResultHeader() {
+        return (
+            <div>
+                <h1 className={"display-6 mx-3"}>Showing search results for
+                    "{props.history.location.search.substring(props.history.location.search.indexOf('=') + 1)}"</h1>
+                <p className={"lead mx-3"}>{resultList.length ? resultList.length + " results found." : "No results found."}</p>
+                <div className={"lead text-end"}>
+                    Sort By: <ButtonGroup>
+                    <Button onClick={(event => sortResults(event.target))}>Address
+                        <span style={{display: showAddAsc ? "inline" : "none"}} id={"sort-asc-address"}> &#8593;</span>
+                        <span style={{display: showAddDesc ? "inline" : "none"}}
+                              id={"sort-desc-address"}> &#8595;</span>
+                    </Button>
+                    <Button onClick={(event => sortResults(event.target))}>Name
+                        <span style={{display: showNameAsc ? "inline" : "none"}} id={"sort-asc-name"}> &#8593;</span>
+                        <span style={{display: showNameDesc ? "inline" : "none"}} id={"sort-desc-name"}> &#8595;</span>
+                    </Button>
+                </ButtonGroup>
+                </div>
             </div>
-            <Row>
-                {resultList.length !== 0 ? resultList.map((result, idx) => {
-                    return <Col sm={4} className={"search-result my-3"} key={idx}>
-                        <Restaurant restaurant={result}/>
-                    </Col>
-                }) : <div>No results found.</div>}
-            </Row>
-        </div>
-    )
+        )
+    }
+
+
+    if (resultList.length !== 0) {
+        return (
+            <div>
+                <ResultHeader/>
+                <Row>
+                    {resultList.map((result, idx) => {
+                        return <Col sm={4} className={"search-result my-3"} key={idx}>
+                            <Restaurant restaurant={result}/>
+                        </Col>
+                    })}
+                </Row>
+            </div>
+        )
+    } else {
+        return (
+            <div className={"container-fluid"}>
+                <ResultHeader/>
+                <div id={"loading"} className={"loading m-auto d-flex justify-content-center align-items-center"}>
+
+                </div>
+            </div>
+        )
+    }
+
 }
 
 export default ResultList;
