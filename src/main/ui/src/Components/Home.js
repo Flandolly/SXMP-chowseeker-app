4266110be10fd258e10089896fdb836bccfe7125
Christina Whethers <flandresamarawr@gmail.com>
2022-03-02 12:58:22 -0500

    Move input form into its own component to be reused

--- src/main/ui/src/Components/Home.js
+++ src/main/ui/src/Components/Home.js
@@ -1,45 +1,14 @@
-import React, {useState} from "react";
-import {Button, FormGroup, Input} from "reactstrap";
-import {Link} from "react-router-dom";
+import React from "react";
+import SearchForm from "./SearchForm";
 
 function Home() {
-
-    // const axios = require("axios");
-    //
-    // useEffect(() => {
-    //     axios.get(`${APIURL}/restaurants`)
-    //         .then(function (response) {
-    //             console.log(response);
-    //         })
-    //         .catch(function (err) {
-    //             console.log(err);
-    //         })
-    // });
-
-    const [input, setInput] = useState("");
-
-    function handleChange(input) {
-        return setInput(input);
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-    }
 
     return (
         <div className={"container-fluid d-flex"}>
             <div className={"d-flex flex-column vh-100 w-100 justify-content-center"}>
                 <h1 className={"text-center d-inline"}>Chowseeker</h1>
                 <h5 className={"text-center"}>Find Your Favorite SF Food Trucks!</h5>
-                <FormGroup>
-                    <Input className={"w-75 mx-auto"} onChange={(event => handleChange(event.target.value))} value={input} type={"search"} name={"search"} placeholder={"Search... (Enter a food or address)"}/>
-                </FormGroup>
-                <Link className={"text-center"} to={{
-                    pathname: "/search",
-                    search: `?query=${input}`
-                }}>
-                    <Button className={"w-25 mx-auto my-2"} onSubmit={(event => handleSubmit(event))} type={"submit"} name={"submitSearch"} color={"primary"}>Submit</Button>
-                </Link>
+                <SearchForm/>
             </div>
         </div>
     );
---
+++ src/main/ui/src/Components/SearchForm.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import {Button, Input, InputGroup, InputGroupAddon} from "reactstrap";
+import {Link, useHistory} from "react-router-dom";
+
+function SearchForm() {
+
+    const [input, setInput] = useState("");
+    const hist = useHistory();
+
+    function handleChange(input) {
+        console.log(input)
+        return setInput(input.target.value);
+    }
+
+    return (
+        <div>
+            <InputGroup className={"w-75 mx-auto"}>
+                <Input onChange={(event) => handleChange(event)}
+                       onKeyPress={(event) => {
+                           if (event.key === "Enter") {
+                               hist.push(`/search?query=${input}`);
+                           }
+                       }}
+                       type={"search"} value={input} name={"search"} placeholder={"Search... (Enter a food or address)"}/>
+                <InputGroupAddon addonType={"prepend"}>
+                    <Link className={"text-center"} to={{
+                        pathname: "/search",
+                        search: `?query=${input}`
+                    }}>
+                        <Button className={""} type={"submit"} name={"submitSearch"} color={"primary"}>Find
+                            Food</Button>
+                    </Link>
+                </InputGroupAddon>
+            </InputGroup>
+        </div>
+    )
+}
+
+export default SearchForm;
