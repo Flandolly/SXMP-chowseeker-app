44bac40e3f16c87d5daabf442e82b44e9d248f83
Christina Whethers <flandresamarawr@gmail.com>
2022-03-03 19:23:42 -0500

    Implement Google API and build more of restaurant component

--- src/main/ui/src/Components/ShowRestaurant.js
+++ src/main/ui/src/Components/ShowRestaurant.js
@@ -8,23 +8,34 @@ function ShowRestaurant(props) {
 
     useEffect(() => {
         const idParam = props.match.url.split("/")
+        console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
         // console.log(idParam)
         axios.get(`${APIURL}/restaurants/${idParam[2]}`)
             .then(function (response) {
                 console.log(response)
+                console.log();
                 setRestaurant(response.data);
             })
     }, [props.match.url])
 
     useEffect(() => {
-        console.log("Got restaurant.")
     }, [restaurant])
 
     return (
         <div>
             <div>
-                <h1 className={"display-3"}>{restaurant.name}</h1>
-                <h1 className={"display-3"}>{restaurant.address}</h1>
+                <div className={"upper-main"}>
+                    <h1 className={"display-4"}>{restaurant.name}</h1>
+                    <h1 className={"display-6"}>{restaurant.address}</h1>
+                    {restaurant.photo ? restaurant.photo : <p className={"lead"}>No photo available.</p>}
+                </div>
+                <div className={"lower-main"}>
+                    <p><b>Serving:</b> {restaurant.foodTypes.replaceAll(":", "-")}</p>
+                    <p><b>Nearby Street Location(s):</b> {restaurant.locationDescription}</p>
+                </div>
+                <p className={"lead"}>Map:</p>
+                <iframe title={"map"} width={600} height={450} loading={"lazy"}
+                        src={`https://www.google.com/maps/embed/v1/place?q=${restaurant.address.replaceAll(" ", "+")}&key=AIzaSyBLhROnrZ1PTEx7LD6CYUh4DPX1ej_twH8`}/>
             </div>
         </div>
     )
